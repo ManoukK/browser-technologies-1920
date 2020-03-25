3b50451ba9b8ee345b51404eb47ded4af49ed805
ManoukK <manouk.kappe21@gmail.com>
2020-03-25 15:13:52 +0100

    formulier verder gemaakt
    nu heb ik twee pagina's en twee tussen pagina's alle antwoorden worden in de url mee gegeven. Ik heb alleen nog geen css toegevoegd

--- server.js
+++ server.js
@@ -8,26 +8,31 @@ const port = process.env.PORT || 2121;
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(express.static('static'));
+//zet naar static bij uploaden
+app.use(express.static('public'));
 
     //shortcuts for queries
     //q1 = naam
     //q2 = email
     //q3 = Telefoonnummer
+    //q4 = geboorte datum
     //q4a = geboorte dag
     //q4b = geboorte maand
     //q4c = geboorte jaar
-
+    //q5 = minor cijfer
+    
 app.get("/savePage1", (req, res) => {
     let url = req.url;
     let queries = {
         q1: "",
         q2: "",
         q3: "",
-        q4a: "",
-        q4b: "",
-        q4c: "",
-        cijfer: "",
+        q4: "",
+        q5: "",
+        q6a: "",
+        q6b: "",
+        q6c: "",
+        q6d: "",
     };
 
     if (Object.entries(req.query).length > 0) {
@@ -35,26 +40,122 @@ app.get("/savePage1", (req, res) => {
     }
 
     res.render('savePage1', { queries, url });
-  });
+});
 
-app.get('/', function(req, res){ 
+app.get("/savePage2", (req, res) => {
+    let url = req.url;
     let queries = {
         q1: "",
         q2: "",
         q3: "",
-        q4a: "",
-        q4b: "",
-        q4c: "",
-        cijfer: "",
+        q4: "",
+        q5: "",
+        q6a: "",
+        q6b: "",
+        q6c: "",
+        q6d: "",
     };
 
     if (Object.entries(req.query).length > 0) {
         queries = req.query;
     }
 
-    res.render('main', { queries });
+    res.render('savePage2', { queries, url });
+});
+
+app.get("/savePage2", (req, res) => {
+    let url = req.url;
+    let queries = {
+        q1: "",
+        q2: "",
+        q3: "",
+        q4: "",
+        q5: "",
+        q6a: "",
+        q6b: "",
+        q6c: "",
+        q6d: "",
+    };
+
+    if (Object.entries(req.query).length > 0) {
+        queries = req.query;
+    }
+
+    res.render('savePage2', { queries, url });
+});
+
+app.get("/savePage3", (req, res) => {
+    let url = req.url;
+    let queries = {
+        q1: "",
+        q2: "",
+        q3: "",
+        q4: "",
+        q5: "",
+        q6a: "",
+        q6b: "",
+        q6c: "",
+        q6d: "",
+    };
+
+    if (Object.entries(req.query).length > 0) {
+        queries = req.query;
+    }
+
+    res.render('savePage3', { queries, url });
+});
+
+app.get('/survey1', function(req, res){ 
+    let queries = {
+        q1: "",
+        q2: "",
+        q3: "",
+        q4: "",
+        q5: "",
+        q6a: "",
+        q6b: "",
+        q6c: "",
+        q6d: "",
+    };
+
+    if (Object.entries(req.query).length > 0) {
+        queries = req.query;
+    }
+
+    res.render('survey1', { queries });
+});
+
+app.get('/survey2', function(req, res){ 
+    let queries = {
+        q1: "",
+        q2: "",
+        q3: "",
+        q4: "",
+        q5: "",
+        q6a: "",
+        q6b: "",
+        q6c: "",
+        q6d: "",
+        
+    };
+
+    if (Object.entries(req.query).length > 0) {
+        queries = req.query;
+    }
+
+    res.render('survey2', { queries });
+});
+
+app.get('/', function(req, res){ 
+    res.render('main');
 });
 
 app.listen(port, host, function() {
     console.log(`Example app listening on port ${port}!`);
 });
+
+//als ik alles terug wil zetten moet ik
+//index in public onderverdelen
+//build css aanzetten
+//server naar static zetten
+//head ccs weg dus alleen /index.css
